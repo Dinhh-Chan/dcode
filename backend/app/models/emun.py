bb53b59db958daa223d8f251641b9a68868bfee1
Dinhh-Chan <dinhtran29092005@gmail.com>
2025-01-14 11:09:22 +0700

    update enum

---
+++ backend/app/models/emun.py
@@ -0,0 +1,20 @@
+import enum 
+class GenderEnum(enum.Enum):
+    male= "male"
+    female ="female"
+    other= "other"
+class RoleEnum(enum.Enum):
+    user="user"
+    admin="admin"
+class SubmissionStatusEnum(enum.Enum):
+    Pending="Pending"
+    Accepted="Accepted"
+    Wrong_Answer="Wrong Answer"
+    Time_Limit_Exeeded="Time Limit Exeeded"
+    Memory_Limit_Exeeded="Memory Limit Exeeded"
+    Runtime_Error="Runtime Error"
+    Compilation_Error="Compilation Error"
+class DiffycultyEnum(enum.Enum):
+    Easy="Dễ"
+    Medium="Trung Bình"
+    Hard="Khó"
