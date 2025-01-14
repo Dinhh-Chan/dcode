addc41e7947f000898441100ab9b71a913786668
Dinhh-Chan <dinhtran29092005@gmail.com>
2025-01-14 14:53:38 +0700

    update orignation table

---
+++ backend/app/models/organizations.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Integer, String, Column, Text 
+from sqlalchemy import relationship
+from datetime import Date
+from . import Base  
+class orginations(Base):
+    __tablename__ = "orginations"
+    id =Column(Integer, primary_key= True , index= True )
+    ten_to_chuc = Column(String(255), nullable= False )
+    mo_ta = Column(Text)
+    ngay_tao = Date 
+    
