09ecbba7734e399ad55c310d25441815912f4555
Dinhh-Chan <dinhtran29092005@gmail.com>
2025-01-14 15:12:19 +0700

    update table difficulties

---
+++ backend/app/models/difficulties.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Integer, String , Column , Enum
+from sqlalchemy import relationship 
+from . import Base 
+from emun import DiffycultyEnum 
+class difficulties(Base):
+    __tablename__= "difficulties"
+    id = Column(Integer, primary_key= True, index= True)
+    do_kho = Column(Enum(DiffycultyEnum), default= DiffycultyEnum.Easy)
+    
