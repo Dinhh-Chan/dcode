798511e1b65d9b1cc9819fecef57ca87002192fe
Dinhh-Chan <dinhtran29092005@gmail.com>
2025-02-07 21:09:58 +0700

    api

--- backend/app/api/user_api.py
+++ backend/app/api/user_api.py
@@ -46,26 +46,28 @@ def login_by_email(user: UserLoginByEmail, db: Session = Depends(get_db)):
     if "error" in result:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=result["error"])
     return result
-
-
+# cập nhật thông tin người dùng
 @router.put("/update/{username}", response_model= UserOut)
 def update_user(username: str, user: UserUpdate, db: Session= Depends(get_db)):
     user_update = update_user(user, db, username)
     if "error" in user_update:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=user_update["error"])
     return user_update 
+#xóa người dùng
 @router.delete("/delete/{username}", response_model= UserOut) 
 def delete_user(username: str, db: Session= Depends(get_db)):
     res = delete_user(db, username)
     if "error" in res :
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=res["error"])
     return res
+#lấy thông tin người dùng
 @router.get("/get/{username}", response_model= UserOut)
 def get_user_infor(username: str , db: Session= Depends(get_db)):
     res= get_user(db, username)
     if res == None :
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail= "user not found")
     return res 
+#lấy thông tin người dùng theo điểm
 @router.get("/ranking", response_model= List[UserOut])
 def get_rank(db: Session = Depends(get_db)):
     res = get_user_order_by_diem(db)
